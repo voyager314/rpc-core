aefbe92c1db6c1caf5ea86e81e292afd9b34208a
voyger314 <1429796980@qq.com>
2026-01-24 18:03:39 +0800

    TcpBufferHandlerWarpper封装处理粘包半包

--- rpc-extend/src/main/java/com/yzy/server/tcp/TcpServerHandler.java
+++ rpc-extend/src/main/java/com/yzy/server/tcp/TcpServerHandler.java
@@ -17,7 +17,43 @@ import java.lang.reflect.Method;
 public class TcpServerHandler implements Handler<NetSocket> {
     @Override
     public void handle(NetSocket netSocket) {
-        netSocket.handler(buffer -> {
+        /*netSocket.handler(buffer -> {
+            ProtocolMessage<RpcRequest> protocolMessage = null;
+            try {
+                protocolMessage = (ProtocolMessage<RpcRequest>) ProtocolMsgDecoder.decode(buffer);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            RpcRequest rpcRequest = protocolMessage.getBody();
+            RpcResponse rpcResponse = new RpcResponse();
+            //根据服务名称，通过反射调用实现方法
+            Class<?> service = LocalRegister.getService(rpcRequest.getServiceName());
+            try {
+                //根据方法名称和请求参数获取实现方法
+                Method method = service.getMethod(rpcRequest.getMethodName(), rpcRequest.getParameterTypes());
+                //调用实现方法获取结果
+                Object result = method.invoke(service.getDeclaredConstructor().newInstance(), rpcRequest.getArgs());
+                //封装调用结果
+                rpcResponse.setData(result);
+                rpcResponse.setMsg("success");
+                rpcResponse.setClazz(method.getReturnType());//返回结果的类型
+            } catch (Exception e) {
+                rpcResponse.setMsg(e.getMessage());
+                throw new RuntimeException(e);
+            }
+            ProtocolMessage.Header header = protocolMessage.getHeader();
+            header.setType((byte) ProtocolMsgTypeEnum.RESPONSE.getValue());
+            ProtocolMessage<RpcResponse> responseProtocolMessage = new ProtocolMessage<>(header, rpcResponse);
+            try {
+                Buffer encode = ProtocolMsgEncoder.encode(responseProtocolMessage);
+                netSocket.write(encode);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });*/
+
+        //处理tcp粘包半包问题
+        TcpBufferHandlerWrapper bufferHandlerWrapper=new TcpBufferHandlerWrapper(buffer -> {
             ProtocolMessage<RpcRequest> protocolMessage = null;
             try {
                 protocolMessage = (ProtocolMessage<RpcRequest>) ProtocolMsgDecoder.decode(buffer);
@@ -51,5 +87,6 @@ public class TcpServerHandler implements Handler<NetSocket> {
                 throw new RuntimeException(e);
             }
         });
+        netSocket.handler(bufferHandlerWrapper);
     }
 }
